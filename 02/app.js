99053c391927a1567a82112cc97a333cd19183b3
Rafał Kochanecki <rafal.kochanecki@gmail.com>
2022-04-04 22:09:22 +0200

    task 2 completed

--- 02/app.js
+++ 02/app.js
@@ -21,3 +21,29 @@ function createRandomArray() {
 function getRandomInteger(min, max) {
     return Math.round(Math.random() * (max-min) + min);
 }
+
+console.log('Pętla for')
+
+// ------------------------ pętla for ---------------------------------
+
+for (let i = 0; i < randomArray.length; i++) {
+    console.log(randomArray[i])
+}
+
+// --------------------------------------------------------------------
+
+console.log('--------------------------------------------------------------------------------------------')
+
+console.log('Pętla forEach')
+
+// ----------------------- pętla forEach ------------------------------
+
+randomArray.forEach(function(number) {
+    console.log(number)
+})
+
+// --------------------------------------------------------------------
+
+console.log('--------------------------------------------------------------------------------------------')
+
+console.log(`Ostatni element tablicy to: ${randomArray[randomArray.length - 1]}`)
