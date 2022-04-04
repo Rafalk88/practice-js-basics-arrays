dfed4fa46bc0272a9d67fe6dfce79a2d710a349f
Rafał Kochanecki <rafal.kochanecki@gmail.com>
2022-04-04 21:52:39 +0200

    task 1 completed

---
+++ 01/app.js
@@ -0,0 +1,11 @@
+users = ['Jan Kowalski',
+        'Dawid Nowak',
+        'Izabela Kwiatkowska',
+        'Alina Krawczyk',
+        'Tadeusz Norek'
+        ]
+
+console.log(`Element 1 tabliczy to: ${users[0]}`)
+console.log(`Element 1 tabliczy to: ${users[2]}`)
+console.log(`Element 1 tabliczy to: ${users[4]}`)
+console.log(`Długość tablicy to: ${users.length}`)
