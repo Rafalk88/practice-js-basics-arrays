3b115f86663e5c061533e9b81e83bf4e84439959
Rafał Kochanecki <rafal.kochanecki@gmail.com>
2022-04-04 22:56:22 +0200

    task 3 completed

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,18 @@
 const n = 24;
 const oddNumbers = [];
+
+const fillArray = function() {
+    for (let i = 0; i <= n; i++) {
+        oddNumbers[i] = i
+    }
+}
+
+function addFilter(element) {
+    return element % 2 === 1
+}
+
+fillArray()
+let newOddNumbers = oddNumbers.filter(addFilter)
+
+console.log(oddNumbers)
+console.log(newOddNumbers)
