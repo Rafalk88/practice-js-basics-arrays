57fa8b33cbc330f1e1a7498af3d3a7eb20c79923
Rafał Kochanecki <rafal.kochanecki@gmail.com>
2022-04-05 00:06:04 +0200

    task 5 completed

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,15 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+const evenArray = function (element) {
+   return element % 2 === 0
+}
+
+const sumArray = function (a, b) {
+    return a + b
+}
+
+const evenNumbers = numbers.filter(evenArray)
+const sumNumbers = evenNumbers.reduce(sumArray)
+
+console.log(`Tablica liczb parzystych: ${evenNumbers}`)
+console.log(`Suma liczb z tablicy: ${sumNumbers}`)
