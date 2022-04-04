d5f09ef505e75560c9b8ea4278eeb8bc8800bb9d
Rafał Kochanecki <rafal.kochanecki@gmail.com>
2022-04-04 23:47:49 +0200

    task 4 completed

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,9 @@
 const years = [1980, 1934, 2002, 2019];
+
+const passedTime = function (year) {
+    const time = new Date()
+    return time.getFullYear() - year
+}
+
+const passTime = years.map(passedTime)
+console.log(passTime)
